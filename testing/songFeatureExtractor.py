2df2088426c8f56e9cfcce4aa76cb2ff1076e5cf
Calvin Studebaker <calstud@stanford.edu>
2014-11-14 17:46:44 -0800

    added LibXtract. Not sure if it was able to fully download all dependencies...

--- testing/songFeatureExtractor.py
+++ testing/songFeatureExtractor.py
@@ -1,10 +1,16 @@
 import soundcloud
 import json
 
-json_data = open("../res/data.json")
+json_data = open("res/data.json")
 data = json.load(json_data)
 
 client = soundcloud.Client(client_id="94931d15df645c93103e0cd600377922")
+
+def addFeature(featureVec, track, featureName):
+	if featureName in track:
+		featureVec[featureName] = track[featureName]
+	else:
+		featureVec[featureName] = None
 
 def getPlaylist(urlstring):
 	playlist = client.get('/resolve', url=urlstring)
@@ -15,17 +21,29 @@ def getPlaylist(urlstring):
 		print '==========================='
 		featureVector = {}
 
-		featureVector["title"] = track['title']
-		featureVector["duration"] = track['duration']
-		featureVector["genre"] = track['genre']
-		featureVector["year"] = track['release_year']
-		featureVector["type"] = track['track_type']
-		featureVector["bpm"] = track['bpm']
-		featureVector["key_signature"] = track['key_signature']
+		# featureVector["title"] = track['title']
+		# featureVector["duration"] = track['duration']
+		# featureVector["genre"] = track['genre']
+		# featureVector["year"] = track['release_year']
+		# featureVector["type"] = track['track_type']
+		# featureVector["bpm"] = track['bpm']
+		# featureVector["key_signature"] = track['key_signature']
 		# featureVector["plays"] = track['playback_count']
 		# featureVector["favorites"] = track['favoritings_count']
 		# featureVector["downloads"] = track['download_count']
-		featureVector["tags"] = track['tag_list']
+		# featureVector["tags"] = track['tag_list']
+
+		addFeature(featureVector, track, 'title')
+		addFeature(featureVector, track, 'duration')
+		addFeature(featureVector, track, 'genre')
+		addFeature(featureVector, track, 'release_year')
+		addFeature(featureVector, track, 'track_type')
+		addFeature(featureVector, track, 'bpm')
+		addFeature(featureVector, track, 'key_signature')
+		addFeature(featureVector, track, 'playback_count')
+		addFeature(featureVector, track, 'favoritings_count')
+		addFeature(featureVector, track, 'download_count')
+		addFeature(featureVector, track, 'tag_list')
 
 		data[playlist.title][track['title']] = featureVector
 
@@ -35,11 +53,7 @@ getPlaylist('https://soundcloud.com/calstud/sets/tom-misch')
 
 # track = client.get('/resolve', url='https://soundcloud.com/toddterje/todd-terje-inspector-norse')
 
-
-
-	# print featureVector
-
-with open('../res/data.json', 'w') as outfile:
+with open('res/data.json', 'w') as outfile:
   json.dump(data, outfile)
 print data
 
