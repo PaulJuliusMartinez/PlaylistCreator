a5d036ddb2cbd8378162e9400bfe10153bfcf2dd
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2014-12-02 21:18:55 -0800

    Least Squares Linear Classifier

---
+++ res/util.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+def dotProduct(d1, d2):
+    if len(d1) < len(d2):
+        return dotProduct(d2, d1)
+    else:
+        return sum(d1.get(f, 0) * v for f, v in d2.items())
+
+def increment(d1, scale, d2):
+    for f, v in d2.items():
+        d1[f] = d1.get(f, 0) + v * scale
+
+def countDiffs(originals, calculated):
+    p1 = originals[0]
+    p2 = originals[1]
+
+    c1 = calculated[:len(p1)]
+    c2 = calculated[-len(p2):]
+
+    c3 = calculated[-len(p1):]
+    c4 = calculated[:len(p2)]
+
+    # Compare p1/c1 and p2/c2
+    numWrong = 0
+    numWrong += sum(0 if song in p1 else 1 for song in c1)
+    numWrong += sum(0 if song in p2 else 1 for song in c2)
+    print numWrong
+
+    # Compare p1/c3 and p2/c4
+    numWrong = 0
+    numWrong += sum(0 if song in p1 else 1 for song in c3)
+    numWrong += sum(0 if song in p2 else 1 for song in c4)
+    print numWrong
--- testing/songFeatureExtractor.py
+++ testing/songFeatureExtractor.py
@@ -7,73 +7,87 @@ playlistPairs = []
 
 client = soundcloud.Client(client_id="94931d15df645c93103e0cd600377922")
 
+# These are songs that can't be downloaded. Don't add them to playlists.
+blacklist = [53433975, 169850810, 59051244, 76989671]
+
+
 def buildPlaylistPairs():
-	currentPair = ()
-	p1 = []
-	p2 = []
-	for i in xrange(len(playlists)):
-		p1 = playlists[i]
-		for j in xrange(i+1, len(playlists)):
-			p2 = playlists[j]
-			for song1 in playlists[i]:
-				for song2 in playlists[j]:
-					if song1 == song2:
-						p1.remove(song1)
-						p2.remove(song1)
-			currentPair = (p1, p2)
-			playlistPairs.append(currentPair)
+    currentPair = ()
+    p1 = []
+    p2 = []
+    for i in xrange(len(playlists)):
+        p1 = playlists[i]
+        for j in xrange(i+1, len(playlists)):
+            p2 = playlists[j]
+            for song1 in playlists[i]:
+                for song2 in playlists[j]:
+                    if song1 == song2:
+                        p1.remove(song1)
+                        p2.remove(song1)
+            currentPair = (p1, p2)
+            playlistPairs.append(currentPair)
 
 def addFeature(featureVec, track, featureName):
-	if featureName in track:
-		featureVec[featureName] = track[featureName]
-	else:
-		featureVec[featureName] = None
+    if featureName in track:
+        featureVec[featureName] = track[featureName]
+    else:
+        featureVec[featureName] = None
 
 def getPlaylist(urlstring):
-	playlist = client.get('/resolve', url=urlstring)
-	tracks = playlist.tracks
-	currentPlaylist = []
-	print "Getting playlist:", playlist.title
-	for track in playlist.tracks:
-		# if not track['streamable']: 
-		# 	print "Skipping song", track['title'], "because it is not streamable"
-		# 	continue
-		if track['id'] in songContent: 
-			print "Skipping song", track['title'], "because it has already been processed"
-			continue
+    playlist = client.get('/resolve', url=urlstring)
+    tracks = playlist.tracks
+    currentPlaylist = []
+    print "Getting playlist:", playlist.title
+    for track in playlist.tracks:
 
-		featureVector = {}
-		addFeature(featureVector, track, 'id')
-		addFeature(featureVector, track, 'created_at')
-		addFeature(featureVector, track, 'user')
-		addFeature(featureVector, track, 'title')
-		addFeature(featureVector, track, 'sharing')
-		addFeature(featureVector, track, 'description')
-		addFeature(featureVector, track, 'label_name')
-		addFeature(featureVector, track, 'license')
-		addFeature(featureVector, track, 'duration')
-		addFeature(featureVector, track, 'genre')
-		addFeature(featureVector, track, 'release_year')
-		addFeature(featureVector, track, 'track_type')
-		addFeature(featureVector, track, 'bpm')
-		addFeature(featureVector, track, 'key_signature')
-		addFeature(featureVector, track, 'comment_count')
-		addFeature(featureVector, track, 'playback_count')
-		addFeature(featureVector, track, 'favoritings_count')
-		addFeature(featureVector, track, 'download_count')
-		addFeature(featureVector, track, 'tag_list')
+        # if not track['streamable']: 
+        #     print "Skipping song", track['title'], "because it is not streamable"
+        #     continue
+        if track['id'] in blacklist:
+            continue
+        try:
+            print track['id']
+            client.get('/tracks/' + str(track['id']))
+        except:
+            print 'Track: ', track['id'], " couldn't be downloaded"
+            continue
+
+        if track['id'] in songContent: 
+            print "Skipping song", track['title'], "because it has already been processed"
+            continue
+
+        featureVector = {}
+        addFeature(featureVector, track, 'id')
+        addFeature(featureVector, track, 'created_at')
+        addFeature(featureVector, track, 'user')
+        addFeature(featureVector, track, 'title')
+        addFeature(featureVector, track, 'sharing')
+        addFeature(featureVector, track, 'description')
+        addFeature(featureVector, track, 'label_name')
+        addFeature(featureVector, track, 'license')
+        addFeature(featureVector, track, 'duration')
+        addFeature(featureVector, track, 'genre')
+        addFeature(featureVector, track, 'release_year')
+        addFeature(featureVector, track, 'track_type')
+        addFeature(featureVector, track, 'bpm')
+        addFeature(featureVector, track, 'key_signature')
+        addFeature(featureVector, track, 'comment_count')
+        addFeature(featureVector, track, 'playback_count')
+        addFeature(featureVector, track, 'favoritings_count')
+        addFeature(featureVector, track, 'download_count')
+        addFeature(featureVector, track, 'tag_list')
 
 
-		#change to download the specific song using soundcloud-downloader.py and extract
-		#features from it here
-		fileLoc = "~/Desktop/playlists/" + playlist.title + "/" + str(track['id']) + ".mp3"
-		featureVector['file_location'] = fileLoc
+        #change to download the specific song using soundcloud-downloader.py and extract
+        #features from it here
+        fileLoc = "~/Desktop/playlists/" + playlist.title + "/" + str(track['id']) + ".mp3"
+        featureVector['file_location'] = fileLoc
 
-		songContent[track['id']] = featureVector
-		currentPlaylist.append(track['id'])
-	if len(currentPlaylist) == 0: return
-	print "Saving playlist:", playlist.title
-	playlists.append(currentPlaylist)
+        songContent[track['id']] = featureVector
+        currentPlaylist.append(track['id'])
+    if len(currentPlaylist) == 0: return
+    print "Saving playlist:", playlist.title
+    playlists.append(currentPlaylist)
 
 
 getPlaylist('https://soundcloud.com/calstud/sets/ski-dock')
@@ -99,10 +113,10 @@ buildPlaylistPairs()
 
 # track = client.get('/resolve', url='https://soundcloud.com/toddterje/todd-terje-inspector-norse')
 
-with open('res/playlists.json', 'w') as outfile:
-  json.dump(playlists, outfile)
-with open('res/songContent.json', 'w') as outfile:
-  json.dump(songContent, outfile)
+#with open('res/playlists.json', 'w') as outfile:
+#  json.dump(playlists, outfile)
+#with open('res/songContent.json', 'w') as outfile:
+#  json.dump(songContent, outfile)
 with open('res/playlistPairs.json', 'w') as outfile:
   json.dump(playlistPairs, outfile)
 
